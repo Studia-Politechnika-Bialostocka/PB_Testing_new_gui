4e552f84993babb73e9bc24017d13db47d0b98d5
lbialczak <lbialczak@grupazpr.pl>
2023-02-26 22:53:12 +0100

    choice_screen to make routing more flexible

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,14 +3,36 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import StepCreator from "./components/step_creator";
-import MainScreen from "./components/main_screen";
+import ChoiceScreen from "./components/choice_screen";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Routes>
-      <Route path="/" element={<MainScreen />} />
+      <Route
+        path="/"
+        element={
+          <ChoiceScreen
+            choices={{
+              "Feature Files": "/feature-files",
+              "Implementation": "/implementation",
+              "Step Creator": "/step-creator",
+            }}
+          />
+        }
+      />
+      <Route
+        path="/feature-files"
+        element={
+          <ChoiceScreen
+            choices={{
+              "Create": "/feature-files/create",
+              "Edit": "/feature-files/edit",
+            }}
+          />
+        }
+      />
       <Route path="/step-creator" element={<StepCreator />} />
     </Routes>
   );
