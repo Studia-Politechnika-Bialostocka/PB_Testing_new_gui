a9f1c5e2bf2660e3e861df198b68e2171c2a5fb2
lbialczak <lbialczak@grupazpr.pl>
2023-02-28 23:26:03 +0100

    get_feature_list and get_feature endpoints

--- src/rest/feature_handlers.py
+++ src/rest/feature_handlers.py
@@ -10,3 +10,16 @@ def create_feature():
     feature_data = request.get_json()
     FeatureService().create_feature(feature_data)
     return {}
+
+
+@features.route("/features/<string:name>", methods=["GET"])
+def get_feature(name: str):
+    feature_name = name.replace("-", " ")
+    feature_data = FeatureService().get_feature(feature_name)
+    return {"feature_data": feature_data}
+
+
+@features.route("/features", methods=["GET"])
+def get_feature_list():
+    feature_list = FeatureService().get_feature_list()
+    return {"feature_list": feature_list}
